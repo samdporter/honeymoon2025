d8cfbf9b2fc66c68e9f649fda5b315f40ecfd767
Sam Porter <sp114@MacBookPro.mynet>
2025-08-18 09:23:09 +0100

    fix wording wrt to high and low prices in StorySection

--- src/components/StorySection.jsx
+++ src/components/StorySection.jsx
@@ -63,7 +63,7 @@ export default function StorySection() {
 
             <p className="text-lg">
               Instead of traditional wedding gifts, help us fund our Italian food adventure! 
-              From a morning espresso (£10) to a Michelin star splurge (£400), every contribution adds flavor to our journey. 
+              From a morning espresso (£10) to a night in Florence (£100), every contribution adds flavor to our journey. 
               Buon viaggio e buon appetito! 🇮🇹
             </p>
           </div>
